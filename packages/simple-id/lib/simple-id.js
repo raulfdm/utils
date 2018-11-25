56f1b9f9bcc22e762c771d93904a7453444c6a37
Raul Melo <melo.raulf@gmail.com>
2018-11-25 12:28:29 +0100

    Fix opt args on main function

--- packages/simple-id/lib/simple-id.js
+++ packages/simple-id/lib/simple-id.js
@@ -1,5 +1,7 @@
 // https://gist.github.com/gordonbrander/2230317
-const simpleId = ({ noUnderscore = false }) => {
+
+export default (opts = {}) => {
+  const { noUnderscore = false } = opts;
   // Math.random should be unique because of its seeding algorithm.
   // Convert it to base 36 (numbers + letters), and grab the first 9 characters
   // after the decimal.
@@ -9,5 +11,3 @@ const simpleId = ({ noUnderscore = false }) => {
 
   return `${!noUnderscore && '_'}${id}`;
 };
-
-export default simpleId;
