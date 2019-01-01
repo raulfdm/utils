cfbe3d73133a1e6437677784c9eb22419281796c
Raul Melo <melo.raulf@gmail.com>
2019-01-01 21:48:36 +0100

    Add more tests to the app

--- packages/coverage-badge-generator/src/index.js
+++ packages/coverage-badge-generator/src/index.js
@@ -1,4 +1,9 @@
+#!/usr/bin/env node
 import '@babel/polyfill';
+import isNumber from 'lodash.isnumber';
+import { resolve } from 'path';
+import { existsSync } from 'fs-extra';
+
 const colorScheme = [
   '#FF0000',
   '#FF1100',
@@ -32,25 +37,59 @@ const colorScheme = [
   '#00FF00'
 ];
 
-export async function extractPercentage() {
-  const coverage = await import('../coverage/coverage-summary.json');
-  const totalStatementsPercentage = coverage.total.statements.pct;
-  return totalStatementsPercentage;
+export function extractPercentage(filePath = '') {
+  const resolvedPath = resolve(
+    process.cwd(),
+    filePath,
+    'coverage-summary.json'
+  );
+  const existsCoverage = existsSync(resolvedPath);
+
+  if (!existsCoverage) {
+    throw new Error(
+      `Could not find coverage-summary.json file at ${resolvedPath}`
+    );
+  }
+
+  return import(resolvedPath)
+    .then(summary => {
+      const totalStatementsPercentage = summary.total.statements.pct;
+      return totalStatementsPercentage;
+    })
+    .catch(console.error);
 }
 
-export function getColor(percentage) {
-  const colorIndex = Math.floor((colorScheme.length * percentage) / 100);
+export function getColorAndPercent(percentage) {
+  if (!isNumber(percentage)) {
+    throw new Error('percentage must be a number');
+  }
+  const roundedPercentage = parseFloat(percentage.toFixed(2));
+  const colorIndex = Math.floor((colorScheme.length * roundedPercentage) / 100);
+
+  // Avoid index 30 === undefined
+  const currentColor =
+    colorIndex === colorScheme.length
+      ? colorScheme[colorIndex - 1]
+      : colorScheme[colorIndex];
+
   return {
-    color: colorScheme[colorIndex].replace('#', ''),
-    percentage
+    color: currentColor.replace('#', ''),
+    percentage: roundedPercentage
   };
 }
 
 export function generateBadge({ percentage, color }) {
+  if (!isNumber(percentage)) {
+    throw new Error('percentage must be a number');
+  }
+
+  if (color.includes('#')) {
+    throw new Error('Invalid color');
+  }
   return `https://img.shields.io/badge/coverage-${percentage}%25-${color}.svg`;
 }
 
-extractPercentage()
-  .then(getColor)
-  .then(generateBadge)
-  .then(console.log);
+extractPercentage('coverage/').then(console.log);
+// .then(getColorAndPercent)
+// .then(generateBadge)
+// .then(console.log);
--- packages/coverage-badge-generator/src/index.test.js
+++ packages/coverage-badge-generator/src/index.test.js
@@ -1,7 +1,143 @@
-import { extractPercentage } from './index';
+import times from 'lodash.times';
+import random from 'lodash.random';
 
-describe('fn: extractPercentage', () => {
-  it('should 1', () => {
-    expect(1).toBe(1);
+import { generateBadge, getColorAndPercent, extractPercentage } from './index';
+
+describe('fn: generateBadge', () => {
+  it('should throw if percentage is not a number ', () => {
+    expect(() => generateBadge({ percentage: false })).toThrow();
+    expect(() => generateBadge({ percentage: 'hie' })).toThrow();
+    expect(() => generateBadge({ percentage: '00' })).toThrow();
+    expect(() => generateBadge({ percentage: {} })).toThrow();
+  });
+  it('should throw if color contains "#"', () => {
+    expect(() => generateBadge({ percentage: 100, color: '#fff' })).toThrow();
+    expect(() =>
+      generateBadge({ percentage: 100, color: 'fff' })
+    ).not.toThrow();
+  });
+
+  [
+    {
+      color: '331222',
+      percentage: 100,
+      get badge() {
+        return `https://img.shields.io/badge/coverage-${this.percentage}%25-${
+          this.color
+        }.svg`;
+      }
+    },
+    {
+      color: '33111F',
+      percentage: 20,
+      get badge() {
+        return `https://img.shields.io/badge/coverage-${this.percentage}%25-${
+          this.color
+        }.svg`;
+      }
+    },
+    {
+      color: '00FFDD',
+      percentage: 1,
+      get badge() {
+        return `https://img.shields.io/badge/coverage-${this.percentage}%25-${
+          this.color
+        }.svg`;
+      }
+    }
+  ].forEach((dataTest, index) => {
+    describe(`case ${index + 1}`, () => {
+      const result = generateBadge(dataTest);
+      it('should return value includes percentage sent', () => {
+        expect(result.includes(dataTest.percentage)).toBeTruthy();
+      });
+
+      it('should return value includes color sent', () => {
+        expect(result.includes(dataTest.color)).toBeTruthy();
+      });
+
+      it('should URL generate match the expected', () => {
+        expect(dataTest.badge).toBe(result);
+      });
+    });
   });
 });
+
+describe('fn: getColorAndPercent', () => {
+  it('should throw if percentage is not a number', () => {
+    expect(() => getColorAndPercent(false)).toThrow();
+    expect(() => getColorAndPercent('hie')).toThrow();
+    expect(() => getColorAndPercent('00')).toThrow();
+    expect(() => getColorAndPercent({})).toThrow();
+    expect(() => getColorAndPercent(100)).not.toThrow();
+  });
+
+  describe('ordinary case', () => {
+    [
+      {
+        percentage: 10,
+        color: 'FF3400'
+      },
+      {
+        percentage: 55,
+        color: 'E5FF00'
+      },
+      {
+        percentage: 26,
+        color: 'FF7B00'
+      },
+      {
+        percentage: 93.3,
+        color: '24FF00'
+      },
+      {
+        percentage: 100,
+        color: '00FF00'
+      }
+    ].forEach(({ percentage, color }) => {
+      describe(`case: percentage: ${percentage} color: ${color}`, () => {
+        const result = getColorAndPercent(percentage);
+        it('should return an object', () => {
+          expect(result).toBeInstanceOf(Object);
+        });
+
+        it('should contain "percentage" property', () => {
+          expect(result).toHaveProperty('percentage');
+        });
+        it('should percentage from result match sent', () => {
+          expect(result.percentage).toBe(percentage);
+        });
+
+        it('should contain "color" property', () => {
+          expect(result).toHaveProperty('color');
+        });
+        it('should "color" from result match from data', () => {
+          expect(result.color).toBe(color);
+        });
+      });
+    });
+  });
+
+  describe('Float numbers', () => {
+    times(5, () => random(100, true)).forEach(randomNumber => {
+      const result = getColorAndPercent(randomNumber);
+      describe(`Percentage => ${randomNumber}`, () => {
+        it('should be rounded by 2', () => {
+          const stringNumber = `${result.percentage}`;
+
+          const isRounded =
+            (stringNumber.length === 4 || stringNumber.length === 5) &&
+            stringNumber.includes('.');
+
+          expect(isRounded).toBeTruthy();
+        });
+      });
+    });
+  });
+});
+
+/* TODO */
+describe('fn: extractPercentage', () => {
+  
+})
+
